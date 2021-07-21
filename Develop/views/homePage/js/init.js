b4571ce88d534739d609e53bd75079bb71cd8d1d
jerismith32 <jerismith32@gmail.com>
2021-07-20 22:20:46 -0400

    updated the front-end

--- Develop/views/homePage/js/init.js
+++ Develop/views/homePage/js/init.js
@@ -6,3 +6,9 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+//Allows the carousel to work as a slider
+$('.carousel.carousel-slider').carousel({
+  fullWidth: true,
+  indicators: true
+});
